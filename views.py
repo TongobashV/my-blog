4e9457ab58be711281d9279808cce58d0b94a594
Vasiliy Tongobash <v.tonhobash@gmail.com>
2024-11-06 23:07:52 +0300

    Improved the blog with advanced features

--- views.py
+++ views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 
@@ -9,7 +9,16 @@ def post_list(request):
     # Постраничная разбивка с 3 постами на страницу
     paginator = Paginator(posts, 3)
     page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # Если page_number не целое число, то
+        # выдать первую страницу
+        posts = paginator.page(1)
+    except EmptyPage:
+        # Если page_number находится вне диапазона, то
+        # выдать последнюю страницу результатов
+        posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 
